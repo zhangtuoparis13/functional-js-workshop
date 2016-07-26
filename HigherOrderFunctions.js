fc48fcd7bb0ddf7d0b67f59d26498464a60754b2
TBtuo <tuo.zhang@telecom-bretagne.eu>
2016-07-26 15:05:42 +0200

    Exo 2/18
    # Task
    
    Implement a function that takes a function as its first argument, a number num as its second argument, then executes the passed in function num times.
    
    Use the boilerplate code given to you below to get started. Most/all future exercises will provide boilerplate.

---
+++ HigherOrderFunctions.js
@@ -0,0 +1,20 @@
+/**
+ * Created by TBtuo on 26/07/16.
+ */
+
+/*
+function repeat(operation, num) {
+    // SOLUTION GOES HERE
+    return operation.num
+}
+
+// Do not remove the line below
+module.exports = repeat*/
+
+function repeat(operation, num) {
+    if (num <= 0) return
+    operation()
+    return repeat(operation, --num)
+}
+
+module.exports = repeat

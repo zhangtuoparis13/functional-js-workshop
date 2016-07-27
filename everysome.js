2206d1b40a6cc0db6bc426295fefd9931ef84330
TBtuo <tuo.zhang@telecom-bretagne.eu>
2016-07-27 19:09:12 +0200

    everyone

---
+++ everysome.js
@@ -0,0 +1,18 @@
+/**
+ * Created by TBtuo on 26/07/16.
+ */
+
+function checkUsersValid(goodUsers) {
+    return function allUsersValid(submittedUsers) {
+        // SOLUTION GOES HERE
+        return submittedUsers.every(function (submitteduser) {
+            return goodUsers.some(function (goodUser) {
+                return submitteduser.id === goodUser.id
+            })
+        })
+    };
+}
+
+
+
+module.exports = checkUsersValid
